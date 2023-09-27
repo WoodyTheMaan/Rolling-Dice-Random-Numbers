3a3d32e62d59e5ad986cdc428233b9658a3f55e1
Josiah Brownlee <buzz72woody@gmail.com>
2023-09-27 10:47:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Making_A_Die_Class;
+
+namespace Rolling_Dice_Random_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1 = new Die();
+            Die die2 = new Die();
+            Console.WriteLine("Click enter to roll the first Die");
+            Thread.Sleep(1000);
+            Console.ReadLine();
+            Console.WriteLine($"You rolled a {die1}");
+            die1.DrawDie();
+            Thread.Sleep(5000);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine(""); 
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Thread.Sleep(2000);
+            Console.WriteLine("Now click enter to roll the second Die");
+            Thread.Sleep(1000);
+            Console.ReadLine();
+            Console.WriteLine($"You rolled a {die2}");
+            die2.DrawDie();
+            Console.WriteLine("Clcik enter twice real fast to leave");
+            Console.ReadLine();
+        }
+    }
+}
