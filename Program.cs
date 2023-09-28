0cd1a20b18763db09372cf903f23d17dee398abf
WoodyTheMaan <buzz72woody@gmail.com>
2023-09-28 10:24:43 -0400

    Added more to program, like breifly downsized sleep commands. (No longer 3 years 999999999999999 * 102)

--- Program.cs
+++ Program.cs
@@ -8,44 +8,14 @@ namespace Rolling_Dice_Random_Numbers
         {
             Die die1 = new Die();
             Die die2 = new Die();
-            Console.WriteLine("Click enter to roll the first Die");
+            Console.WriteLine("WELCOME!!!!! Click enter to roll the first Die");
             Thread.Sleep(1000);
             Console.ReadLine();
             Console.WriteLine($"You rolled a {die1}");
             die1.DrawDie();
-            Thread.Sleep(5000);
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine(""); 
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
+            Console.Clear();
+            Thread.Sleep(500);
             Console.WriteLine("Now click enter to roll the second Die");
             Thread.Sleep(1000);
             Console.ReadLine();
@@ -53,6 +23,9 @@ namespace Rolling_Dice_Random_Numbers
             die2.DrawDie();
             Console.WriteLine("Clcik enter twice real fast to leave");
             Console.ReadLine();
+
+
+
         }
     }
 }
